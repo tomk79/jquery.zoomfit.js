99538b54f779d90bb341daf24ca69d00ea643b06
Tomoya Koyanagi <tomk79@gmail.com>
2013-05-17 19:38:58 +0900

    ざっくり実装してみた。Chromeでは動いてる。

--- scripts/jquery.zoomfit.js
+++ scripts/jquery.zoomfit.js
@@ -3,22 +3,59 @@
  * @author Tomoya Koyanagi <tomk79@gmail.com>
  */
 (function($){
+	var _targetItems = [];
+	var _defWidth = 0;
+	var _maxZoom = 1;
 
-	var targetItems = [];
+	function fit_elm(item){
+		var tw = item.elm.parent().width();
+		var uw = item.width;
 
-	/*
-	* init();
-	*/
-	$.init = function(){
-		targetItems = [];
-	}// init();
+		var zoom = tw/uw;
+		item.elm.css('zoom', (zoom<=_maxZoom?zoom:_maxZoom));
+	}
+
+	$.zoomfit = new (function(){
+
+		/*
+		* init();
+		*/
+		this.init = function(defWidth){
+			_targetItems = [];
+			if(defWidth){_defWidth = defWidth;}
+			return this;
+		}// init();
 
 
-	/*
-	* addElements();
-	*/
-	$.addElements = function(){
-	}// addElements();
+		/*
+		* addElements();
+		*/
+		this.addElements = function(target, width){
+			$(target)
+				.each(function(){
+					var item = {
+						elm: $(this) ,
+						width: (width?width:_defWidth)
+					};
+					if(item.elm[0].attributes['data-std-width']){
+						item.width = item.elm[0].attributes['data-std-width'].value;
+						item.width = Number(item.width);
+					}
 
+					fit_elm(item);
+					_targetItems.push( item );
+				})
+			;
+			return this;
+		}// addElements();
+
+	})();
+
+	// イベントをセット
+	$(window).bind('resize', function(){
+		for(var index in _targetItems){
+			fit_elm(_targetItems[index]);
+		}
+	});
 
 })(jQuery);
